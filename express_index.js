dfc5009d4e802320c2188600c2a420db2d44f647
Citrus <zhouyou421@126.com>
2017-06-16 11:34:32 +0800

    orthers router

--- express_index.js
+++ express_index.js
@@ -3,32 +3,16 @@
  */
 const app = require('express')();
 const server = require('http').Server(app);
-const io = require('./server/socket')(server);
-const fs = require('fs');
+require('./server/socket')(server); //使用 socket.io
+const router = require('./server/router');
 
+//配置
 const config = require('./server/config');
+
+//使用路由
+router(__dirname ,app);
+
 
 server.listen(config.port, () => {
     console.log(`server listening prot ${config.port}`);
 });
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
-
-//处理其它资源
-app.use((req, res) => {
-    let url = req.url;
-    fs.stat(__dirname + url, function(err){
-        if(err){
-            console.log(`404 not found ${url}`);
-            res.sendStatus(404);
-        }else{
-            //console.log('ok');
-            res.sendFile(__dirname + url);
-        }
-    })
-});
-
-
--- server/router.js
+++ server/router.js
@@ -1,3 +1,28 @@
 /**
  * Created by Citrus on 2017/6/16.
  */
+const fs = require('fs');
+const router = function (root, app) {
+    const rootDir = root;
+
+    app.get('/', function (req, res) {
+        res.sendFile(rootDir + '/index.html');
+    });
+
+    //处理其它资源
+    app.use((req, res) => {
+        let url = req.url;
+        fs.stat(rootDir + url, function (err) {
+            if (err) {
+                console.log(`404 not found ${url}`);
+                res.sendStatus(404);
+            } else {
+                //console.log('ok');
+                res.sendFile(rootDir + url);
+            }
+        })
+    });
+
+};
+
+module.exports = router;
